afdd848d6c22bd28d90a33b29b3125e6ab27d8a1
seungwooj <seungwoo.aaron@gmail.com>
2018-04-29 03:49:19 +0900

    selfstudy_book

---
+++ webscraping_with_python/chp1_첫번째웹스크레이퍼.py
@@ -0,0 +1,47 @@
+# Crawling 101 - urllib.request & BeautifulSoup
+
+## Using BeautifulSoup
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+html = urlopen("http://www.pythonscraping.com/pages/page1.html")
+bsObj = BeautifulSoup(html.read(), "html.parser")
+print(bsObj.h1)
+
+## More complex but safe way to code1 : try & except
+
+from urllib.request import urlopen
+from urllib.request import HTTPError
+from bs4 import BeautifulSoup
+
+try:
+    html = urlopen("http://www.pythonscraping.com/pages/page1.html")
+except:
+    print(e)
+
+## More complex but safe way to code2 : use functions
+
+from urllib.request import urlopen
+from urllib.request import HTTPError
+from bs4 import BeautifulSoup
+
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+
+    try:
+        bsObj = BeautifulSoup(html.read(), "html.parser")
+        title = bsObj.body.h1
+    except AttributeError as e:
+        return None
+    return title
+
+title = getTitle("http://www.pythonscraping.com/pages/page1.html")
+if title == None:
+    print("Title could not be found")
+else:
+    print(title)
+
+
