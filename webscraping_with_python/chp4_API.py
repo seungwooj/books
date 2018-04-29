10039f3ce665836999f28bd02676d87ec8bf674d
seungwooj <seungwoo.aaron@gmail.com>
2018-04-30 03:21:45 +0900

    chp4_API

--- webscraping_with_python/chp4_API.py
+++ webscraping_with_python/chp4_API.py
@@ -1,14 +1,23 @@
+
+
 # get : 웹서버에 정보를 요청
 # post : 웹서버에 정보를 보냄 -> 저장 요청 (ex) log-in : id & password)
 # put : 웹서버의 정보를 업데이트 (ex> 사용자 이메일 주소를 업데이트, 변경)
 # delete : 웹서버의 정보를 삭제
 
 
+
+
+
+## wikipedia에서 사용자 url정보 가져오기
+
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import datetime
 import random
 import re
+import json
+
 random.seed(datetime.datetime.now())
 
 
@@ -44,14 +53,45 @@ def getHistoryIPs(pageUrl): #
 
 links = getLinks("/wiki/Python_(programming_language)")
 
+
+
+## 코드 합칠 때 while문 주석처리 뒤의 함수 실행 X
+# while (len(links) > 0):
+#     for link in links:
+#         print("-------------------")
+#         historyIPs = getHistoryIPs(link.attrs["href"])
+#         for historyIP in historyIPs:
+#             print(historyIP)
+#
+#     newLink = links[random.randint(0, len(links)-1)].attrs["href"]
+#     links = getLinks(newLink)
+
+## freegeoip.net에서 IP주소의 country_code 가져오기
+
+def getCountry(ipAddress):
+    try:
+
+        response = urlopen("http://freegeoip.net/json/"+ipAddress).read().decode('utf-8')
+
+    except HTTPError:
+
+        return None
+
+    responseJson = json.loads(response)
+    return responseJson.get("country_code")
+
+
+links = getLinks("/wiki/Python_(programming_language)")
+
+
 while (len(links) > 0):
     for link in links:
-        print("-------------------")
+        print("---------------")
         historyIPs = getHistoryIPs(link.attrs["href"])
         for historyIP in historyIPs:
-            print(historyIP)
+            country = getCountry(historyIP)
+            if country is not None:
+                print(historyIP+" is from "+country)
 
     newLink = links[random.randint(0, len(links)-1)].attrs["href"]
     links = getLinks(newLink)
-
-
